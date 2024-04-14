f21577f51f79bb9c0a47e23eac9a98a47de3ccd6
ankitbrain <ankit.m@brainwired.in>
2024-04-14 20:00:28 +0530

    File structure Created

---
+++ template.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+    '.github/workflows/.gitkeep',# this folder for entire configuration of CI/CD pipeline
+     # git keep for to push empty folder over the github
+    'src/__init__.py',
+    'src/components/__init__.py',
+    'src/components/data_ingestion.py',
+    'src/components/data_transformation.py',
+    'src/components/model_trainer.py',
+    'src/components/evaluation.py',
+    'src/pipeline/__init__.py',
+    'src/pipeline/training_pipeline.py',
+    'src/pipeline/prediction_pipeline.py',
+    'src/utils/__init__.py',
+    'src/utils/utils.py', # for utility
+    'src/logger/logging.py',
+    'src/exception/exception.py',
+    'tests/unit/__init__.py',
+    'tests/integration/__init__.py',
+    'init_setup.sh',
+    'requirements.txt',
+    'requirements_dev.txt',
+    'setup.py',
+    'setup.cfg',
+    'pyproject.toml',
+    'tox.ini',     # test different test-case in local development env 
+    'experiment/experiments.ipynb',
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    file__dir ,file_name = os.path.split(filepath)
+
+    if file__dir!="": # becuase in above list we mention file name only then file_dir will empty
+        #this line code will create folder for files
+        os.makedirs(file__dir,exist_ok=True)  
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath))==0:
+        # create file with extension (for file folder already crated in above code)
+        with open(filepath,'w') as f:
+            pass
