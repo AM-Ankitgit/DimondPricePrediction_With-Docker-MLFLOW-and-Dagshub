b95b5dcba16cafbc508ef05ccbefc89b40367702
AM-Ankitgit <mahalleankit007@gmail.com>
2024-07-18 20:35:18 +0530

    Change updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,2 +1,19 @@
-# this file is use to right the linux command to run code file 
-# here we will write multiple linux command and will run this file to excute all operation
+#!/bin/bash
+
+echo [$(date)]: "START"
+
+# Source the Conda initialization script
+echo [$(date)]: "Initializing Conda"
+source /c/Users/BW_ML_1/anaconda3/etc/profile.d/conda.sh
+
+echo [$(date)]: "Creating conda env with python 3.8"
+conda create --prefix ./.env python=3.8 -y
+
+echo [$(date)]: "Activating env"
+source activate ./.env
+
+echo [$(date)]: "Installing dev requirements"
+pip install -r requirements_dev.txt
+
+echo [$(date)]: "END"
+
